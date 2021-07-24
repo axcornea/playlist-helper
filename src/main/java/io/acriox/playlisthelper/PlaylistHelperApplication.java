ee27b5189db2fec68266fe42a768bb098f214b59
Alex Cornea <cornea.alex@yandex.com>
2021-07-24 13:06:13 +0300

    Bootstrap project using Spring Initializer

---
+++ src/main/java/io/acriox/playlisthelper/PlaylistHelperApplication.java
@@ -0,0 +1,13 @@
+package io.acriox.playlisthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/acriox/playlisthelper/PlaylistHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acriox.playlisthelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
